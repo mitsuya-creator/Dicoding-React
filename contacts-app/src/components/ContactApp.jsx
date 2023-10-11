1b593d401311453d7b4c8a05cb294bf20d98ee19
@code <jj4068851@gmail.com>
2023-10-11 15:55:42 +0700

    Add: feature delete button

--- contacts-app/src/components/ContactApp.jsx
+++ contacts-app/src/components/ContactApp.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import ContactList from "./ContactList";
 import { getData } from "../utils/data";
 
-function ContactApp() {
-    const contacts = getData();
-
-    return (
-        <div className="contact-app">
-            <h1>Daftar Kontak</h1>
-            <ContactList contacts={contacts} />
-        </div>
-    )
+class ContactApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: getData()
+        }
+        this.onDeleteButton = this.onDeleteButton.bind(this)
+    }
+    onDeleteButton(id) {
+        const contacts = this.state.contacts.filter(contact => contact.id !== id);
+        this.setState({ contacts })
+    }
+    render() {
+        return (
+            <div className="contact-app" >
+                <h1>Daftar Kontak</h1>
+                <ContactList contacts={this.state.contacts} onDelete={this.onDeleteButton} />
+            </div>
+        )
+    }
 }
 
 export default ContactApp;
--- contacts-app/src/components/ContactItem.jsx
+++ contacts-app/src/components/ContactItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ContactItemBody from "./ContactItemBody";
 import ContactItemImage from "./ContactItemImage";
+import DeleteButton from "./DeleteBtn";
 
-function ContactItem({ imgUrl, name, tag }) {
+function ContactItem({ imgUrl, name, tag, id, onDelete }) {
     return (
         <div className="contact-item">
             <ContactItemImage imgUrl={imgUrl} />
             <ContactItemBody name={name} tag={tag} />
+            <DeleteButton id={id} onDelete={onDelete} />
         </div>
     )
 }
