27a9333240a51f179178ae7c420fa08bf3b2daad
@code <jj4068851@gmail.com>
2023-10-18 14:27:18 +0700

    Add: Add Cardlist utility

--- personal-notes-starter/src/components/App.jsx
+++ personal-notes-starter/src/components/App.jsx
@@ -4,15 +4,22 @@ import ActionButton from './ActionButton';
 import CardList from './CardList';
 import Footer from './Footer';
 import AddNote from './AddNote';
+import { getInitialData, showFormattedDate } from "../utils";
 
 export default function App() {
+    const initialData = getInitialData();
     return (
         <>
             <Navbar />
             <ActionButton />
-            <CardList />
+            <div className="parent-card">
+                {
+                    initialData.map(data => <CardList title={data.title} body={data.body} createdAt={data.createdAt} archive={data.archived} showDateFormatted={showFormattedDate} key={data.id} />)
+                }
+            </div>
             <AddNote />
             <Footer />
         </>
     )
 }
+
