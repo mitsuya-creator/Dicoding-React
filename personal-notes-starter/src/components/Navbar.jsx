6816012412912d453ccdc0b2c09ccf9fa48e8432
@code <jj4068851@gmail.com>
2023-10-23 13:25:51 +0700

    Add: style navbar on mobile

--- personal-notes-starter/src/components/Navbar.jsx
+++ personal-notes-starter/src/components/Navbar.jsx
@@ -6,9 +6,7 @@ function Navbar(props) {
         <>
             <nav className="flex-justify-start nav">
                 <h1 className="text-color-white">Notes</h1>
-                <section>
-                    <SearchBox onSearch={props.onSearch} />
-                </section>
+                <SearchBox onSearch={props.onSearch} />
             </nav>
         </>
     )
